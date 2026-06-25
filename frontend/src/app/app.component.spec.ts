e5b37947601a4848810e9b41cb46f3f7838431de
Jörn Franke <security-oss@zuinnote.eu>
2026-06-25 14:15:27 +0200

    feat: angular material 22

--- frontend/src/app/app.component.spec.ts
+++ frontend/src/app/app.component.spec.ts
@@ -1,6 +1,4 @@
 import { TestBed } from "@angular/core/testing";
-import { Router } from "@angular/router";
-import { RouterTestingHarness } from "@angular/router/testing";
 import { provideRouter } from "@angular/router";
 import { AppComponent } from "./app.component";
 
